c9ccd8c457d5c3794256f8e73d5b8ee4be7947e8
Cristh72 <guerrerocristhian.coder@gmail.com>
2024-09-10 22:41:24 +0200

    Spring Security

---
+++ backend/src/main/java/com/reforestart/backend/controller/ResourceController.java
@@ -0,0 +1,23 @@
+package com.reforestart.backend.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/resource")
+public class ResourceController {
+
+    @GetMapping("/user")
+    public ResponseEntity<String> read_user (Authentication authentication){
+        return ResponseEntity.ok("The user can read."+authentication.getAuthorities());
+    }
+
+    @PostMapping("/user")
+    public ResponseEntity<String> write_user (Authentication authentication){
+        return ResponseEntity.ok("The user can write."+authentication.getAuthorities());
+    }
+}
