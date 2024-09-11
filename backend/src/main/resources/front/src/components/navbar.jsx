3eb03dfdddc8a2caf3819c743eae88988134cb3e
tonycq <anthonycq2020@gmail.com>
2024-09-11 10:28:02 -0300

    correcion errores buttom

--- backend/src/main/resources/front/src/components/navbar.jsx
+++ backend/src/main/resources/front/src/components/navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { UserContext } from '../components/userContex'; // Importa el contexto de usuario
-import Button from './Button';
+import Button from './button';
 import "../App.css";
 
 function Navbar() {
